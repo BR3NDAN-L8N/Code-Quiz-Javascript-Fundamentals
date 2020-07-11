33e364f6a857c568491d4415f3eb3d2a662adb62
Game Maker <br3ndan.l8n@gmail.com>
2020-07-11 18:57:32 -0400

    Added basic HTML elements. Declared variables in JS

---
+++ script.js
@@ -0,0 +1,9 @@
+startButton = document.querySelector('start-quiz');
+questionDiv = document.querySelector('question-div');
+
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    
+}
