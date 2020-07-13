0882cca8fa8aa868d2d9d92564f3953b23a5d0bb
Game Maker <br3ndan.l8n@gmail.com>
2020-07-13 16:10:39 -0400

    Made an array of objects for the questions/answers. Workgin on a way to determine correct answer selected.

--- script.js
+++ script.js
@@ -3,42 +3,85 @@ const questionDiv = document.querySelector('#question-div');
 const nextButton = document.querySelector("#next-button");
 const finishButton = document.querySelector("#finish-button");
 
-
+let questions = [
+    // {
+    //     "question": `${createdQuestion}`,
+    //     "correctAnswer": `${createdCorrectAnswer}`,
+    //     "option1": `${createdOption1}`,
+    //     "option2": `${createdOption2}`,
+    //     "option3": `${createdOption3}`
+    // },
+    {
+        question: `This is the question`,
+        correctAnswer: `Correct`,
+        option1: `wrong`,
+        option2: `wrong`,
+        option3: `wrong`
+    }
+]
 
 function startQuiz() {
     createQuestionDiv();
 } 
 
 function createQuestionDiv() {
-    let questionNumber = 0;
-    let totalQuestions = 0;
-    let answerOption = ["Q1", "Q2", "Q3", "Q4"];
+    let questionNumber = 1;
+    let answerOption = ["Option1", "Option2", "Option3", "Option4"];
 
     questionDiv.innerHTML = 
     `
     <div id="question-div">
             <div class="card">
-                <div class="card-title">Question ${questionNumber} of ${totalQuestions}</div>
+                <div class="card-title">Question ${questionNumber} of ${questions.length}</div>
                 <div class="card-body">
-                    
-                    <label for="question1"><input type="radio" name="question1" id="question-1"> ${answerOption[0]}</label>
-                    <br />
-                    <label for="question2"><input type="radio" name="question2" id="question-2"> ${answerOption[1]}</label>
-                    <br />
-                    <label for="question3"><input type="radio" name="question3" id="question-3"> ${answerOption[2]}</label>
-                    <br />
-                    <label for="question4"><input type="radio" name="question4" id="question-4"> ${answerOption[3]}</label>
-                    <br />
-                    <button class="btn btn-dark" type="submit" id="next-button">Next ></button>
-                    <button class="btn btn-dark hide" id="finish-button">Finish</button>
-                    
+
+                    <p><strong>${questions[0].question}</strong></p>
+                    <form>
+                        <label for="option"><input type="radio" name="option" id="option"> <strong>A:</strong> ${questions[0].correctAnswer}</label>
+                        <br />
+                        <label for="option"><input type="radio" name="option" id="option"> <strong>B:</strong> ${questions[0].option1}</label>
+                        <br />
+                        <label for="option"><input type="radio" name="option" id="option"> <strong>C:</strong> ${questions[0].option2}</label>
+                        <br />
+                        <label for="option"><input type="radio" name="option" id="option"> <strong>D:</strong> ${questions[0].option3}</label>
+                        <br />
+                        <button class="btn btn-dark" type="button" id="next-button" onclick="checkAnswers()">Next ></button>
+                        <button class="btn btn-dark hide" id="finish-button">Finish</button>
+                    </form>
                 </div>
     `;
-
+    // window.onload = function () {
+    //     nextButton.addEventListener('click', function (e) {
+    //         checkAnswers();
+    //     })
+    // }
+    
+    
 }
 
 
-startButton.addEventListener('click', function(e) {
+
+
+function checkAnswers() {
+    const answer = document.input.namedItem('option');
+    console.log(`answer that was checked was ${answer}`);
+        // if (answer) {
+        //     if (answers[i].value === questions[0].correctAnswer){
+        //         console.log(`correct!`);
+        //     } else {
+        //         console.log(`wrong...`);
+        //     }
+        // }
+}
+
+
+startButton.addEventListener('click', function (e) {
     startButton.classList.toggle("hide");
     startQuiz();
 })
+
+
+
+// function fillQuestionContent() {
+
+// }
