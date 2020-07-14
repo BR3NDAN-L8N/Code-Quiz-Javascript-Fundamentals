c7e0d26dd114ac5b9612cbf084484d7db29e0714
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-14 00:00:28 -0400

    Rearranged code, swapped some stuff between HTML and JS. Swapped to JQuery. Added ability to check which radio button is selected for users answer.

--- script.js
+++ script.js
@@ -1,84 +1,69 @@
-const startButton = document.querySelector('#start-quiz');
-const questionDiv = document.querySelector('#question-div');
-const nextButton = document.querySelector("#next-button");
-const finishButton = document.querySelector("#finish-button");
+$(document).ready(function() {
+    const startButton = $('#start-quiz');
+    const questionDiv = $('#question-div');
+    const nextButton = $("#next-button");
+    const finishButton = $("#finish-button");
+    const questionNumber = $("#question-number");
+    let newQuestionNumber = 0;
+    const totalQuestions = $("#total-questions");
+    const theQuestion = $("#question-description");
+    const option1 = $('#option1');
+    const option2 = $('#option2');
+    const option3 = $('#option3');
+    const option4 = $('#option4');
 
-let questions = [
-    // {
-    //     "question": `${createdQuestion}`,
-    //     "correctAnswer": `${createdCorrectAnswer}`,
-    //     "option1": `${createdOption1}`,
-    //     "option2": `${createdOption2}`,
-    //     "option3": `${createdOption3}`
-    // },
-    {
-        question: `This is the question`,
-        correctAnswer: `Correct`,
-        option1: `wrong`,
-        option2: `wrong`,
-        option3: `wrong`
+    let questions = [
+        // {
+        //     "question": `${createdQuestion}`,
+        //     "correctAnswer": `${createdCorrectAnswer}`,
+        //     "option1": `${createdOption1}`,
+        //     "option2": `${createdOption2}`,
+        //     "option3": `${createdOption3}`
+        // },
+        {
+            question: `This is the question`,
+            correctAnswer: `Correct`,
+            option1: `wrong`,
+            option2: `wrong`,
+            option3: `wrong`
+        }
+    ]
+
+    // function startQuiz() {
+    //     populateQuestionDiv();
+    // } 
+
+    function populateQuestionDiv() {
+        questionDiv.removeClass('hide');
+        questionNumber.text(newQuestionNumber);
+        let answerOption = ["Option1", "Option2", "Option3", "Option4"];
+        
+        option1.text(answerOption[0]);
+        option2.text(answerOption[1]);
+        option3.text(answerOption[2]);
+        option4.text(answerOption[3]);
     }
-]
+            // if (answer) {
+            //     if (answers[i].value === questions[0].correctAnswer){
+            //         console.log(`correct!`);
+            //     } else {
+            //         console.log(`wrong...`);
+            //     }
+            // }
 
-function startQuiz() {
-    createQuestionDiv();
-} 
-
-function createQuestionDiv() {
-    let questionNumber = 1;
-    let answerOption = ["Option1", "Option2", "Option3", "Option4"];
-
-    questionDiv.innerHTML = 
-    `
-    <div class="container" id="question-div">
-            <div class="card justify-content-center ml-md-4 mr-md-4">
-                <div class="card-title text-center">Question ${questionNumber} of ${questions.length}</div>
-                <div class="card-body">
-
-                    <p><strong>${questions[0].question}</strong></p>
-                    <form>
-                        <label for="option"><input type="radio" name="option" id="option"> <strong>A:</strong> ${questions[0].correctAnswer}</label>
-                        <br />
-                        <label for="option"><input type="radio" name="option" id="option"> <strong>B:</strong> ${questions[0].option1}</label>
-                        <br />
-                        <label for="option"><input type="radio" name="option" id="option"> <strong>C:</strong> ${questions[0].option2}</label>
-                        <br />
-                        <label for="option"><input type="radio" name="option" id="option"> <strong>D:</strong> ${questions[0].option3}</label>
-                        <br />
-                        <button class="btn btn-dark float-right" type="button" id="next-button" onclick="checkAnswers()">Next ></button>
-                        <button class="btn btn-dark hide" id="finish-button">Finish</button>
-                    </form>
-                </div>
-    `;
-    // window.onload = function () {
-    //     nextButton.addEventListener('click', function (e) {
-    //         checkAnswers();
-    //     })
-    // }
-    
-    
-}
+            startButton.on('click', function () {
+                startButton.toggle(".hide");
+                populateQuestionDiv();
+            })
+        
+            nextButton.on('click', function () {
+                const userSelection = $("input[name='option']:checked").val();
+                console.log(userSelection);
+                newQuestionNumber++;
+            })
+    });
 
 
-
-
-function checkAnswers() {
-    const answer = document.input.namedItem('option');
-    console.log(`answer that was checked was ${answer}`);
-        // if (answer) {
-        //     if (answers[i].value === questions[0].correctAnswer){
-        //         console.log(`correct!`);
-        //     } else {
-        //         console.log(`wrong...`);
-        //     }
-        // }
-}
-
-
-startButton.addEventListener('click', function (e) {
-    startButton.classList.toggle("hide");
-    startQuiz();
-})
 
 
 
