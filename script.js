8b2e2212fc7047acd4c3040a2cf823ac085207c8
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-19 20:12:41 -0400

    done refactoring?

--- script.js
+++ script.js
@@ -1,122 +1,86 @@
 $(document).ready(function () {
   //  Vars linking to HTML
-  const startButton = $('#start-quiz');
-  const questionDiv = $('#question-div');
-  const nextButton = $("#next-button");
-  const finishButton = $("#finish-button");
-  const questionNumber = $("#question-number");
-  const totalQuestions = $("#total-questions");
-  const theQuestionDescription = $("#question-description");
-  const theQuestionCode = $("#question-code");
-  const scoreDiv = $("#score-div");
-  const option1 = $('#option1');
-  const option2 = $('#option2');
-  const option3 = $('#option3');
-  const option4 = $('#option4');
-  const newScoreDisplay = $("#new-score-display");
-  const saveScoreButton = $("#save-score-button");
-  const usersNameInput = $("#users-name");
-  const timeDisplay = $("#time-display");
-  const topScoreDisplay = $("#top-score-display");
+  const startButton = $('#start-quiz');  // button that starts the quiz, hides upon pressing
+  const questionDiv = $('#question-div');  // The div holding the question, answers, next/finish button, and timer.
+  const nextButton = $("#next-button");  // next button, for going to next question
+  const finishButton = $("#finish-button");  // button shown on last question only, event bring up score-div
+  const questionNumber = $("#question-number");  // The number of the current question user is on
+  const totalQuestions = $("#total-questions");  // Total number of questions the user has to endure
+  const theQuestionDescription = $("#question-description");  // Where we display the question being asked
+  const theQuestionCode = $("#question-code");  // Where we put code-snippets that go with question
+  const scoreDiv = $("#score-div");  // The entire div where scores are displayed, used to hide/un-hide
+  const option1 = $('#option1');  // where the question's possible answers are displayed
+  const option2 = $('#option2');  // same ^^^
+  const option3 = $('#option3');  // same ^^^
+  const option4 = $('#option4');  // same ^^^
+  const newScoreDisplay = $("#new-score-display");  // where the users new score is displayed when they are prompted to input their name
+  const saveScoreButton = $("#save-score-button");  // where the button is for saving user Name and Score
+  const usersNameInput = $("#users-name");  // where user inputs their name after taking quiz
+  const timeDisplay = $("#time-display");  // where time is displayed
+  const topScoreDisplay = $("#top-score-display");  // where top scores are displayed
   //  vars for storage
-  const getSavedScoresLocal = JSON.parse(localStorage.getItem("userScores"));
+  const getSavedScoresLocal = JSON.parse(localStorage.getItem("userScores"));  // get scores from local storage
   //  vars that change
-  let newQuestionNumber = 0;
-  let score = 0;
-
-
-
-  if (!Array.isArray(getSavedScoresLocal)) {  // does an array already exist in local storage?
-    getSavedScoresLocal = [];  // if not then we make it exist!
-  }
-
-  saveScoreButton.on("click", function (event) {
-    event.preventDefault();
-    let name = usersNameInput.val();  // temporarily storing users entered name into a var
-    usersNameInput.val("");  // clearing the input field user typed their name
-    const userRecordObject = {  // setting the users name and score into a new object
-      name: name,
-      score: score
-    }
-    getSavedScoresLocal.push(userRecordObject);  // adding the above new object to saved scores array
-    localStorage.setItem("userScores", JSON.stringify(getSavedScoresLocal));  // setting the new array to local storage
-
-    putOnPage();  // puts the new list of scores onto the page from local storage
-  });
-
-  function putOnPage() {
-
-    topScoreDisplay.empty();  // empties the section where we display the top scores
-    const savedScores = getSavedScoresLocal;
-
-    if (!Array.isArray(savedScores)) {  // does an array already exist in local storage?
-      savedScores = [];  // if not then we make it exist!
-    }
-    
-    for (let i = 0; i < savedScores.length; i++) {  // render our savedScores to the page
-      const newParagraph = $("<p>").text(`Name: ${savedScores[i].name}, Score: ${savedScores[i].score}`);  // put each combo of name and score into a p-tag
-      topScoreDisplay.prepend(newParagraph);  // Prepend so most recent score shows first
-    }
-  }
-  
+  let newQuestionNumber = 0;  // 2 uses, 1: display to the user so they know what Q they're on. 2: before increment on new Q, used as an index point for the array of questions
+  let score = 0;  // Score that the user accumulates through taking the quiz
   let timerInterval;  // this is used for setting/resetting the time interval that runs the timer
   let timerRunning = false;  // this will be checked before certain timer functions happen
 
   
   const timer = {  // Our timer object
-
     time: 30,  // initiate time
+    
     reset: function () {  // reset timer
       timer.time = 30;  // reset timer.time back to 30
       timeDisplay.text("00:30");  // reset time in the timer display
     },
 
-    start: function () {
+    start: function () {  // start timer
       // DONE: Use setInterval to start the count here and set the timer to running.
-      if (!timerRunning) {
-        timerInterval = setInterval(timer.count, 1000);
-        timerRunning = true;
+      if (!timerRunning) {  // is timerRunning is false,
+        timerInterval = setInterval(timer.count, 1000);  // we set the timer to count by 1 second
+        timerRunning = true;  // set timerRunning to true
       }
     },
 
-    stop: function () {
+    stop: function () {  // stop timer
       clearInterval(timerInterval);  //Clear timer interval
       timerRunning = false;  // timer no longer running so set to false
     },
 
-    count: function () {
+    count: function () {  // how the timer counts
       timer.time--;  //decrement time by 1
       timeDisplay.text(timer.timeConverter(timer.time));  // Pass the current time (timer.time) into timeConverter() and display the time
     },
 
-    timeConverter: function (time) {
+    timeConverter: function (time) {  // convert time from seconds into minutes and seconds for display purposes
       let minutes = Math.floor(time / 60);  // time, in seconds, divided by 60 to give us number of minutes
-      let seconds = time - (minutes * 60);  // multiply minutes by 60 to give it's amount of seconds, and subtract from the time we have left
+      let seconds = time - (minutes * 60);  // multiply minutes by 60 to give it's amount of seconds, and subtract from the total number of seconds that time is (time is >= minutes)
 
-      if (seconds < 10) {
-        seconds = `0${seconds}`;
+      if (seconds < 10) {  // if secs < 10,
+        seconds = `0${seconds}`;  // we want seconds to display as 00 - 09
       }
 
-      if (minutes === 0) {
-        minutes = `00`;
+      if (minutes === 0) {  // if mins = 0
+        minutes = `00`;  // we want mins to show as 00
       }
-      else if (minutes < 10) {
-        minutes = `0${minutes}`;
+      else if (minutes < 10) {  // if mins < 10,
+        minutes = `0${minutes}`;  // we want minutes to display as 00 - 09
       }
 
-      return `${minutes}:${seconds}`;
+      return `${minutes}:${seconds}`;  // return the display of time as **:**
     }
   };
 
-  let questionsArray = [
+  let questionsArray = [  // Array of objects makes up the questions, all objects are structured the same so only 1st has comments
     {
-      question: {
-        description: `What is the output of the following code?`,
-        codeSnippet: `var fruits = ["Banana", "Orange", "Apple", "Mango"]; \
-                              $("output").text = fruits.sort();`
+      question: {  // questions have 2 parts,
+        description: `What is the output of the following code?`,  // Description with question
+        codeSnippet: `let fruits = ["Banana", "Orange", "Apple", "Mango"]; \  
+                              $("output").text = fruits.sort();`  // code snippet to aid question
       },
-      correctAnswer: `["Apple", "Banana", "Mango", "Orange"]`,
-      wrongAnswers: {
+      correctAnswer: `["Apple", "Banana", "Mango", "Orange"]`,  // The correct answer
+      wrongAnswers: {                                           // The wrong answers
         option1: `["Orange", "Mango", "Banana", "Apple"]`,
         option2: `undefined`,
         option3: `null`
@@ -137,8 +101,39 @@ $(document).ready(function () {
     },
   ]
 
+  if (!Array.isArray(getSavedScoresLocal)) {  // does an array already exist in local storage?
+    getSavedScoresLocal = [];  // if not then we make it exist!
+  }
 
+  saveScoreButton.on("click", function (event) {  // when user presses the button to submit their Name and Score
+    event.preventDefault();  // We pave our own path
+    let name = usersNameInput.val();  // temporarily storing users entered name
+    usersNameInput.val("");  // clearing the input field user typed their name
+    const userRecordObject = {  // setting the users name and score into a new object
+      name: name,
+      score: score
+    }
+    getSavedScoresLocal.push(userRecordObject);  // adding the above new object to saved scores array
+    localStorage.setItem("userScores", JSON.stringify(getSavedScoresLocal));  // setting the new array, now a string, to local storage
 
+    displayScores();  // displays the new list of scores from local storage
+  });
+
+  // Displays the current scores saved in local storage, if any exist
+  function displayScores() {
+    topScoreDisplay.empty();  // empties the section where we display the top scores
+    const savedScores = getSavedScoresLocal;
+
+    if (!Array.isArray(savedScores)) {  // does an array already exist in local storage?
+      savedScores = [];  // if not then we make it exist!
+    }
+    
+    for (let i = 0; i < savedScores.length; i++) {  // render our savedScores to the page
+      const newParagraph = $("<p>").text(`Name: ${savedScores[i].name}, Score: ${savedScores[i].score}`);  // put each combo of name and score into a p-tag
+      topScoreDisplay.prepend(newParagraph);  // Prepend so most recent score shows first
+    }
+  }
+  
   // Randomize the order of the questions in the questionsArray[array]
   function randomQuestionOrder(questionsArray) {
     for (i = 0; i < questionsArray.length; i++) {
@@ -167,29 +162,28 @@ $(document).ready(function () {
       nextButton.addClass('hide');  // and hide the "next" button as we have no more questions left and want to "finish" the quiz
     }
   }
-
-  startButton.on('click', function () {
-    startButton.addClass("hide");
-    randomQuestionOrder(questionsArray);
-    populateQuestionDiv();
-    timer.start();
+  // EVENT LISTENERS
+  startButton.on('click', function () {  // When user hits the "start quiz" button,
+    startButton.addClass("hide");  // hide the start button,
+    randomQuestionOrder(questionsArray);  // randomize the order of the questions,
+    populateQuestionDiv();  // display first question,
+    timer.start();  // and start the timer
   })
 
-  nextButton.on('click', function () {
-    const userSelection = $("input[name='option']:checked").val();
-    if (userSelection === "option1") {
+  nextButton.on('click', function () {  // when user hits the next button,
+    const userSelection = $("input[name='option']:checked").val();  // we assign the value of the selected answer,
+    if (userSelection === "option1") {  // check that the selection is correct, if so we add to the users score
       score += 20;
-      return score;
     }
-    populateQuestionDiv();
-    timer.reset();
+    populateQuestionDiv();  // next button was pressed, so we load up the next question in the array
+    timer.reset();  // reset the timer for the new question
   })
 
-  finishButton.on('click', function () {
-    questionDiv.addClass('hide');
-    scoreDiv.removeClass('hide');
-    newScoreDisplay.text(`Your score was ${score}!`);
-    timer.stop();
+  finishButton.on('click', function () {  // When user hits the finish button,
+    questionDiv.addClass('hide');  // hide the div containing question stuff because we don't need to see it anymore
+    scoreDiv.removeClass('hide');  // un-hide the score-div, so user can access score related stuff; save name/score, see other scores
+    newScoreDisplay.text(`Your score was ${score}!`);  // display the score the user just got with enthusiasm! 
+    timer.stop();  // stop the timer so it isn't running in the background
 
   })
 
